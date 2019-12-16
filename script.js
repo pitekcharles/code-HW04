f8bcea078ab9f52b4f3e3a6969eb86e2c96bb4c9
Noodles <charles.pitek@gmail.com>
2019-12-16 13:08:03 -0700

    made it so the start quiz button is hidden once the quiz is started

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 var timeEl = document.querySelector("#quizTime");
 var quizButton = document.getElementById("startQuiz");
+var highScore = document.getElementById("highScore");
+var score = 0;
+var storedHighScore = 0;
 
 function quizTimer(){
     var quizCountDown = 60;
     var timerInterval = setInterval(function(){
-        timeEl.textContent = quizCountDown + " seconds left to finish the quiz!";    
+        timeEl.textContent = "Time: " + quizCountDown;    
         quizCountDown--;
 
         if(quizCountDown === 0) {
             clearInterval(timerInterval);
+            timeEl.textContent = "";
             //add post quiz functionallity here likely a function call out to post quiz functions
         }
     }, 1000)
 }
 
+function quizRun(){
+    
+}
+
 quizButton.addEventListener("click", function(){
+    quizButton.style.display = "none"
     quizTimer();
 });
+
+highScore.textContent = "High Score: " + storedHighScore;
